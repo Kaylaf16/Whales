10d32176700e79f724fc0b22a8eb4d3dd6a51994
Kayla Ford <Kayla.ford@mail.citytech.cuny.edu>
2018-04-28 17:00:47 -0400

    added functionality in backend

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from app import db, login
+from flask_login import UserMixin
+from flask_login import login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class Users(UserMixin ,db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    name = db.Column(db.String(64), index=True)
+    location = db.Column(db.String(120), index=True, unique=True)
+    password = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
