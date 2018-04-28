8ddea748cc1a99164ff6f4f3a2aba8f3bc12f7ca
Kayla Ford <Kayla.ford@mail.citytech.cuny.edu>
2018-04-28 07:50:23 -0400

    added front end

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,13 @@ from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
 def index():
      return render_template('landingpage.html')
+
+@app.route('/signup')
+def signup():
+     return render_template('signup.html')
+
+@app.route('/signin')
+def signin():
+     return render_template('signin.html')
