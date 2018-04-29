8b995e6be316c47bfc0e01210a13ffbd8ad07fb3
George Nwankwo <gnwankwo03@gmail.com>
2018-04-29 02:27:26 -0400

    added map functionality

--- app/routes.py
+++ app/routes.py
@@ -22,55 +22,55 @@ def signin():
 
 @app.route('/skillmap')
 def skillmap():
-    rng = pd.date_range('1/1/2011', periods=7500, freq='H')
-    ts = pd.Series(np.random.randn(len(rng)), index=rng)
 
-    graphs = [
-        dict(
-            data=[
-                dict(
-                    x=[1, 2, 3],
-                    y=[10, 20, 30],
-                    type='scatter'
-                ),
-            ],
-            layout=dict(
-                title='first graph'
-            )
-        ),
+    df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_us_cities.csv')
+    df.head()
 
-        dict(
-            data=[
-                dict(
-                    x=[1, 3, 5],
-                    y=[10, 50, 30],
-                    type='bar'
-                ),
-            ],
-            layout=dict(
-                title='second graph'
-            )
-        ),
+    df['text'] = df['name'] + '<br>Population ' + (df['pop']/1e6).astype(str)+' million'
+    limits = [(0,2),(3,10),(11,20),(21,50),(50,3000)]
+    colors = ["rgb(0,116,217)","rgb(255,65,54)","rgb(133,20,75)","rgb(255,133,27)","rgb(255,220,0)"]
+    cities = []
+    scale = 50000
 
-        dict(
-            data=[
-                dict(
-                    x=ts.index,  # Can use the pandas data structures directly
-                    y=ts
-                )
-            ]
+    for i in range(len(limits)):
+        lim = limits[i]
+        df_sub = df[lim[0]:lim[1]]
+        city = dict(
+            type = 'scattergeo',
+            locationmode = 'USA-states',
+            lon = df_sub['lon'],
+            lat = df_sub['lat'],
+            text = df_sub['text'],
+            sizemode = 'diameter',
+            marker = dict( 
+                size = df_sub['pop']/scale, 
+                color = colors[i],
+                line = dict(width = 2,color = 'black')
+            ),
+            name = '{0} - {1}'.format(lim[0],lim[1]) )
+        cities.append(city)
+
+    layout = dict(
+            title = '2014 US city populations<br/>(Click legend to toggle traces)',
+            showlegend = True,
+            geo = dict(
+                scope='usa',
+                projection   = dict( type='albers usa' ),
+                showland     = True,
+                landcolor    = 'rgb(217, 217, 217)',       
+                subunitwidth = 1,
+                countrywidth = 1,
+                subunitcolor = "rgb(255, 255, 255)",
+                countrycolor = "rgb(255, 255, 255)"           
+            ),  
         )
-    ]
-
-    # Add "ids" to each of the graphs to pass up to the client
-    # for templating
-    ids = ['graph-{}'.format(i) for i, _ in enumerate(graphs)]
+        
+    fig = dict( data=cities, layout=layout )
 
     # Convert the figures to JSON
     # PlotlyJSONEncoder appropriately converts pandas, datetime, etc
     # objects to their JSON equivalents
-    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
     return render_template('layouts/index.html',
-                           ids=ids,
                            graphJSON=graphJSON)
